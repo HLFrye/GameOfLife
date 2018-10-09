881045fc3ab4e1e7142fd70802721456bc11565b
HLFrye <HLFrye@github>
2018-10-08 23:12:51 -0500

    Moved some classes around

---
+++ GameView.cs
@@ -0,0 +1,83 @@
+using System;
+using Terminal.Gui;
+
+class GameView: View 
+{
+    private readonly Func<int, int, bool> _charFunc;
+    private Point _focus;
+
+    public GameView(int x, int y, int width, int height)
+        : base(new Rect(x, y, width, height))
+        {
+            var rng = new Random();
+            _charFunc = (a, b) => (rng.Next() % 2) == 0;
+            _focus = new Point(1, 1);
+        }
+
+    public override bool CanFocus { get; set; } = false;
+
+    public override bool ProcessKey(KeyEvent keyEvent)
+    {
+        switch (keyEvent.Key)
+        {
+            case Key.CursorUp:
+                if (_focus.Y > 1)
+                {
+                    _focus.Y -= 1; 
+                }
+                SetNeedsDisplay();
+                return true;
+            case Key.CursorDown:
+                if (_focus.Y < (Frame.Bottom - 1))
+                {
+                    _focus.Y += 1;
+                }
+                SetNeedsDisplay();
+                return true;
+            case Key.CursorLeft: 
+                if (_focus.X > 1)
+                {
+                    _focus.X -= 1;
+                }
+                SetNeedsDisplay();
+                return true;
+            case Key.CursorRight:
+                if (_focus.X < (Frame.Right - 1))
+                {
+                    _focus.X += 1;
+                }
+                SetNeedsDisplay();
+                return true;
+            default: 
+                return false;
+        }
+    }
+
+    public override void Redraw(Rect region)
+    {
+        Clear();
+        for (var y = region.Top; y < region.Bottom; ++y)
+        {
+            for (var x = region.Left; x < region.Right; ++x) 
+            {
+                if (HasFocus && _focus.X == x && _focus.Y == y)
+                {
+                    Driver.SetAttribute(ColorScheme.Focus);
+                }
+                else
+                {
+                    Driver.SetAttribute(ColorScheme.Normal);
+                }
+                Move(x, y);
+                var draw = _charFunc(x, y) ? 'x' : ' ';
+                Driver.AddRune(draw);
+            }
+        }
+    }
+
+    internal void TakeFocus(Window win)
+    {
+        CanFocus = true;
+        win.SetFocus(this);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,78 +2,6 @@
 using System.Threading;
 using Terminal.Gui;
 
-delegate bool GetChar(int x, int y);
-
-class BitView: View 
-{
-    private readonly GetChar _charFunc;
-    private Point _focus;
-
-    public BitView(int x, int y, int width, int height, GetChar charFunc)
-        : base(new Rect(x, y, width, height))
-        {
-            _charFunc = charFunc;
-            _focus = new Point(10, 10);
-        }
-
-    public override bool CanFocus => true;
-
-    public override bool ProcessKey(KeyEvent keyEvent)
-    {
-        switch (keyEvent.Key)
-        {
-            case Key.CursorUp:
-                if (_focus.Y > 1)
-                {
-                    _focus.Y -= 1; 
-                }
-                return true;
-            case Key.CursorDown:
-                if (_focus.Y < (Frame.Bottom - 1))
-                {
-                    _focus.Y += 1;
-                }
-                return true;
-            case Key.CursorLeft: 
-                if (_focus.X > 1)
-                {
-                    _focus.X -= 1;
-                }
-                return true;
-            case Key.CursorRight:
-                if (_focus.X < (Frame.Right - 1))
-                {
-                    _focus.X += 1;
-                }
-                return true;
-            default: 
-                return false;
-        }
-    }
-
-    public override void Redraw(Rect region)
-    {
-        Clear();
-        for (var y = region.Top; y < region.Bottom; ++y)
-        {
-            for (var x = region.Left; x < region.Right; ++x) 
-            {
-                if (_focus.X == x && _focus.Y == y)
-                {
-                    Driver.SetAttribute(ColorScheme.Focus);
-                }
-                else
-                {
-                    Driver.SetAttribute(ColorScheme.Normal);
-                }
-                Move(x, y);
-                var draw = _charFunc(x, y) ? 'x' : ' ';
-                Driver.AddRune(draw);
-            }
-        }
-    }
-}
-
 class Demo {
     static void NewFile()
     {
@@ -150,15 +78,25 @@ class Demo {
 
         var rng = new Random();
         var frame = new FrameView(gameRect, "Life");
-        var gameView = new BitView(0, 0, gameRect.Width - 2, gameRect.Height - 2, (x, y) => (rng.Next() % 2) == 0);
+        var gameView = new GameView(0, 0, gameRect.Width - 2, gameRect.Height - 2);
         frame.Add(gameView);
         win.Add(frame);        
 
+        editBtn.Clicked += () => gameView.TakeFocus(win);
+
         var autoEvent = new AutoResetEvent(false);
-        var started = true;
+        var started = false;
         startBtn.Clicked += () => 
         {
             started = !started;
+            if (started)
+            {
+                startBtn.Text = "Stop";
+            }
+            else
+            {
+                startBtn.Text = "Start";
+            }
         };
 
         var stateTimer = new Timer(x => 
