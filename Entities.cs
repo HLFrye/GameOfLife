bec8b2aaa9f5a537ee2f641e13178bc3c5e4af90
HLFrye <HLFrye@github>
2018-10-09 23:20:14 -0500

    Added more entities

--- Entities.cs
+++ Entities.cs
@@ -1,11 +1,15 @@
 using System;
+using System.IO;
+using System.Reflection;
 
 public static class Entities
 {
-  public static Entity StillLife => new Entity();
-  public static Entity Oscillator => new Entity();
-  public static Entity Glider => new Entity();
-  public static Entity GliderGun => new Entity();
-  public static Entity PufferTrain => new Entity();
-  public static Entity RPentomino => new Entity();
+  private static Stream GetEmbeddedStream(string name) => Assembly.GetAssembly(typeof(Entities)).GetManifestResourceStream($"gui-cs-demo.Entities.{name}");
+
+  public static Entity StillLife => new Entity("Still Life", new []{"OO", "OO"});
+  public static Entity Oscillator => new Entity("Oscillator", new []{"OOO"});
+  public static Entity Glider => new Entity(GetEmbeddedStream("Glider.cells"));
+  public static Entity GliderGun => new Entity(GetEmbeddedStream("GliderGun.cells"));
+  public static Entity PufferTrain => new Entity(GetEmbeddedStream("HiveNudger2.cells"));
+  public static Entity RPentomino => new Entity(GetEmbeddedStream("RPentomino.cells"));
 }
--- Entity.cs
+++ Entity.cs
@@ -5,15 +5,62 @@ using Terminal.Gui;
 
 public class Entity
 {
+  private static IList<Point> ReadLines(IList<string> lines)
+  {
+    var cells = new List<Point>();
+    int y = 0;
+    foreach (var line in lines)
+    {
+      int x = 0;
+      foreach (var c in line)
+      {
+        switch (c)
+        {
+          case 'O':
+            cells.Add(new Point(x, y));
+            break;
+          default:
+            break;
+        }
+        x++;
+      }
+      y++;
+    }
+    return cells;
+  }
+
   public Entity() {}
 
+  public Entity(string name, string[] lines)
+  {
+    Name = name;
+    Cells = ReadLines(lines);
+  }
   public Entity(Stream stream)
   {
-
+    using (var reader = new StreamReader(stream))
+    {
+      Name = reader.ReadLine().Substring(7);
+      var map = new List<string>();
+      while (!reader.EndOfStream)
+      {
+        var line = reader.ReadLine();
+        if (line.StartsWith("!"))
+        {
+          if (!string.IsNullOrEmpty(Description))
+            Description += "\n";
+          Description += line.Substring(0);
+        }
+        else
+        {
+          map.Add(line);
+        }
+      }
+      Cells = ReadLines(map);
+    }
   }
 
   public string Name { get; private set; }
   public string Description { get; private set; }
-  public string URL { get; private set; }
   public IList<Point> Cells { get; private set; }
 }
